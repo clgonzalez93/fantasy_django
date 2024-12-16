4dd2c015b41ee9e99aa484750982e6069621f8ab
clgonzalez <clara.gonzalez28@gmail.com>
2024-12-16 21:41:16 +0000

    added csrf token, user registration now working

--- register/views.py
+++ register/views.py
@@ -5,5 +5,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register(response):
-    form = UserCreationForm()
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+
     return render(response, "register/register.html", {"form": form})
